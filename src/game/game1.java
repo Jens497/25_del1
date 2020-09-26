6208713dc14a53c94eb4abf18ef2f8b00ca07409
Jens <jens_jens_kloster@hotmail.com>
2020-09-26 12:37:18 +0200

    "Første commit"

---
+++ src/game/game1.java
@@ -0,0 +1,7 @@
+package game;
+
+public class game1 {
+    public int add(int o, int i){
+        return o+i;
+    }
+}
---
+++ src/test/test1.java
@@ -0,0 +1,10 @@
+package test;
+
+
+public class test1 {
+
+    public static void main(String args[]){
+        game.game1 game = new game.game1();
+        System.out.println(game.add(1,4));
+    }
+}
