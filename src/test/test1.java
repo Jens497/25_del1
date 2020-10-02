f1fa2bb9ff475b7ed5c9722fd652701ea80ef3e7
Adam <adamharald440@gmail.com>
2020-10-02 13:04:13 +0200

    Fixed naming in test

--- src/test/test1.java
+++ src/test/test1.java
@@ -1,13 +1,12 @@
 package test;
 
 import java.lang.*;
+
 import game.Beaker;
 
 public class test1 {
 
     public static void main(String[] args) {
-        game.game1 game = new game.game1();
-        System.out.println(game.add(1, 4));
     }
 
     Beaker beaker = new game.Beaker(2);
@@ -23,7 +22,7 @@ public class test1 {
         int[] obs = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int a;
         // Get time in milliseconds before the for loop.
-        long time =System.currentTimeMillis();
+        long time = System.currentTimeMillis();
         // Roll the dice 1008 times and store data in the array.
         for (int i = 0; i < 1008; i++) {
             beaker.roll();
@@ -31,13 +30,16 @@ public class test1 {
             // Adds 1 to the specific outcome in the array.
             obs[a - 2]++;
             // Counts both true and false outcomes of isIdentical (needed for chiSquare)
-            if (beaker.isIdentical()){obs[11]++;}
-            else {obs[12]++;}
+            if (beaker.isIdentical()) {
+                obs[11]++;
+            } else {
+                obs[12]++;
+            }
         }
         // Calculate how long it took to run the for loop.
         time = System.currentTimeMillis() - time;
         // Divide it by the number of rolls, to have an estimate on how long each roll took.
-        double perRoll = (double) (time)/1008.;
+        double perRoll = (double) (time) / 1008.;
         System.out.println("Upper bound on average time per roll: " + perRoll);
         // Return the array of observations.
         return obs;
@@ -63,31 +65,29 @@ public class test1 {
         for (int i = 0; i < 11; i++) {
             a = (exp[i] - obs[i]);
             a = a * a;
-            chSq += (double) (a)/(double) (exp[i]);
+            chSq += (double) (a) / (double) (exp[i]);
         }
         System.out.println("ChiSquared sample = " + chSq);
         // threshold is the solution x to the equation P_dice(X>x)=0.95, with 10 DoF. See report.
         double threshold = 21.0261;
         // Print a conclusion of the statistical test. The test should fail about 5% of the time.
-        if (threshold > chSq){
+        if (threshold > chSq) {
             System.out.println("The test confirms the dice as credible with a significance level of 5%.");
-        }
-        else {
+        } else {
             System.out.println("The test rejects the dice as credible with a significance level of 5%.");
         }
         // Repeat the process for the isIdentical data.
-        for (int i = 0; i < 2; i++){
-            a = exp[i+12] - obs[i+12];
+        for (int i = 0; i < 2; i++) {
+            a = exp[i + 12] - obs[i + 12];
             a = a * a;
-            chSq += (double) (a)/(double) (exp[i+12]);
+            chSq += (double) (a) / (double) (exp[i + 12]);
         }
         // threshold here is different, because we change to 1 DoF, down from 10. See report.
         threshold = 3.8415;
 
-        if (threshold > chSq){
+        if (threshold > chSq) {
             System.out.println("The test confirms double rolls credible with a significance level of 5%.");
-        }
-        else {
+        } else {
             System.out.println("The test rejects double rolls credible with a significance level of 5%.");
         }
     }
